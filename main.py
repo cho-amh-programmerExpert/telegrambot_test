6a4cffb8d3c691b8c1c52674892f1eb050ae983d
amh-cho <102322216+cho-amh-programmerExpert@users.noreply.github.com>
2024-06-23 13:32:45 +0330

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
-TOKEN = open("./token.txt")
+TOKEN = open("./token.txt", "r").read()
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hello! I am the glorious leader of Persia.')
